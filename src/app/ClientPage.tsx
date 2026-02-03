004c0b8f54060c0a50c6530fd0666ab8fd804b39
성찬홍 <bkn367@naver.com>
2026-02-03 12:17:35 +0900

    feat(layout/page) : 랜딩 페이지 백그라운드 스파크 코어형태로

--- src/app/ClientPage.tsx
+++ src/app/ClientPage.tsx
@@ -8,13 +8,28 @@ interface ClientPageProps {
     section3Slot: ReactNode;
 }
 
+import { SparklesCore } from "@/components/ui/sparkles";
+
 export default function ClientPage({ section2Slot, section3Slot }: ClientPageProps) {
     return (
-        <div className=" mt-[100px] pt-[50px] borderfont-sans grid grid-rows-[auto_auto_auto_auto] items-center justify-items-center min-h-screen p-0 pb-20 gap-16 sm:p-0">
-            {/* <Section1 /> */}
-            {section2Slot}
-            <Section4 />
-            {section3Slot}
-        </div>
+        <>
+            <div className="fixed top-0 left-0 w-full h-full pointer-events-none z-0 bg-black">
+                <SparklesCore
+                    id="tsparticlesfullpage"
+                    background="black"
+                    minSize={0.6}
+                    maxSize={1.4}
+                    particleDensity={100}
+                    className="w-full h-full"
+                    particleColor="#FFFFFF"
+                />
+            </div>
+            <div className="relative z-10 mt-[100px] pt-[50px] borderfont-sans grid grid-rows-[auto_auto_auto_auto] items-center justify-items-center min-h-screen p-0 pb-20 gap-16 sm:p-0 text-white">
+                {/* <Section1 /> */}
+                {section2Slot}
+                <Section4 />
+                {section3Slot}
+            </div>
+        </>
     );
 }
